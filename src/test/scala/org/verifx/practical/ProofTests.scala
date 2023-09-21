fbdb4b7e36de08c62974365ba916faa28e98351f
alvieupnext <alvarovargasguerrero@hotmail.com>
2023-09-21 15:25:12 +0200

    Relaxed conditions for DotKernels

--- src/test/scala/org/verifx/practical/ProofTests.scala
+++ src/test/scala/org/verifx/practical/ProofTests.scala
@@ -47,9 +47,9 @@ class ProofTests extends FlatSpec with Prover {
 
     "DotKernel" should "be a CRDT" in {
       val proof = ("DotKernel", "is_a_CvRDT")
-//      val res = rejectForModel(proof)
-//      println(res.toString())
-          prove(proof)
+      val res = rejectForModel(proof)
+      println(res.toString())
+//          prove(proof)
     }
 
 //    "DotContext" should "be a CRDT" in {
